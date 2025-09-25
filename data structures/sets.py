36a9a42cad516aeb44ef253903dcf79c914da07e
SakshiAngre <angresakshi653@gmail.com>
2025-09-26 00:01:56 +0530

    learned data structures

---
+++ data structures/sets.py
@@ -0,0 +1,14 @@
+#Sets are unordered, unique collections (no duplicates).
+s={2,23,3,44}
+b={4,23,6,77}
+fruits = {"apple", "banana", "cherry"}
+
+print(b.add(48))
+print(b.remove(23))
+print(b.discard(10))
+print(b.pop())
+
+
+print(s.union(b))     
+print(s.intersection(b))  
+print(s.difference(b))
---
+++ data structures/tuples.py
@@ -0,0 +1,15 @@
+"""Tuples are ordered but immutable collections (cannot be changed after creation)."""
+tup=(10, 20, 30)
+
+print(tup[0]) 
+
+a=(4,)
+"""to store single element in tuple, (,) is used because (paranthisis) is used for group together    """
+
+#tuple unpacking
+
+a=(3,2,4,5)
+
+a1,b2,c3,d4=a
+
+print(a1,b2,c3,d4)
