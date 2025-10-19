ecde1006499364d5ea3fc2d1df8b7d75ce2e08e8
SakshiAngre <angresakshi653@gmail.com>
2025-10-19 23:27:34 +0530

    working on os

--- Advance_python/30_static&class_methods.py
+++ Advance_python/30_static&class_methods.py
@@ -2,15 +2,32 @@ class Employee:
 
     company="cisco"
 
-    def __init__(self,name,salary):
+    def __init__(self, name, salary):
         self.name=name
         self.salary=salary
 
-    #instance method
+    #instance method(default)
     def info(self):
         return f"name of emp is:{self.name},salary is:{self.salary}"
+        print(info)
+
+    @staticmethod
+    def sum(a,b):
+        return a+b
+    
+    @classmethod
+    def print_company(cls):
+        print(cls.company)
+
+    @classmethod
+    def change_company(cls,new_company):
+        cls.comapany=new_company
+
 
 e=Employee("gorge",830043)
 e1=Employee("Genelia",832435)
 print(Employee.company)
-print(Employee.name)#throw error because, it is instance attribute ans i am trying to access it with class name
+#print(Employee.name)#throw error because, it is instance attribute ans i am trying to access it with class name
+print(Employee.company)
+e1.change_company("Acer")
+print(Employee.company)
