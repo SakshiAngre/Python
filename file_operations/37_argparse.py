4e3391ce5805180bad177a84134db4505d76f8be
Sakshi Angre <angresakshi653@gmail.com>
2025-11-06 19:30:29 +0530

    command line utilities

---
+++ file_operations/37_argparse.py
@@ -0,0 +1,26 @@
+import argparse
+
+parser = argparse.ArgumentParser(description="simple calculator")
+
+parser.add_argument("num1",type=float,help="firstnum")
+parser.add_argument("num2",type=float,help="secondnum")
+parser.add_argument("operation",choices=["add","sub","div","mul"],help="operation to perform")
+
+args= parser.parse_args()
+
+print(args)
+
+if(args.operation == "add"):
+    print(f"the result is {args.num1 + args.num2}")
+
+elif(args.operation == "sub"):
+    print(f"the result is {args.num1 - args.num2}")
+
+elif(args.operation == "mul"):
+    print(f"the result is {args.num1 * args.num2}")
+
+elif(args.operation == "div"):
+    print(f"the result is {args.num1 / args.num2}")
+
+else:
+    print("some error occured")
